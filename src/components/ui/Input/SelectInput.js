d1e2124ef1fbe348e0d273fb2bc7f27eadf3ce22
Zach Saffrin <zach@zachsaffrin.com>
2019-10-24 22:00:49 -0400

    Sort selection choices by name

--- src/components/ui/Input/SelectInput.js
+++ src/components/ui/Input/SelectInput.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { arrayOf, func, shape, string } from "prop-types";
 
+import { sortBy } from "../../../utils/dataUtils";
+
 const SelectInput = ({ id, value, onChange, choices }) => {
+  const sortedChoices = sortBy(choices, "name");
+
   return (
     <select id={id} value={value} onChange={onChange}>
       <option value=""></option>
-      {choices.map(choice => (
+      {sortedChoices.map(choice => (
         <option value={choice.id} key={choice.id}>
           {choice.name}
         </option>
