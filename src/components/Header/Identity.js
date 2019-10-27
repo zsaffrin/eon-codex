fd36af3d6efd487a3c8d0023cbdcc0613067b743
Zach Saffrin <zach@zachsaffrin.com>
2019-10-27 17:08:53 -0400

    Use history hook, extract logout function

--- src/components/Header/Identity.js
+++ src/components/Header/Identity.js
@@ -1,13 +1,18 @@
 import React, { useContext } from "react";
-import { withRouter } from "react-router-dom";
+import { useHistory, withRouter } from "react-router-dom";
 
 import { useCurrentUser } from "../../hooks/authHooks";
 import { FirebaseContext } from "../../contexts/firebaseContext";
 import { Button, Loading } from "../ui";
 
-const Identity = ({ history }) => {
+const Identity = () => {
   const firebase = useContext(FirebaseContext);
   const [user, userLoaded] = useCurrentUser(firebase);
+  const history = useHistory();
+
+  const handleLogout = () => {
+    firebase.logout();
+  };
 
   return !userLoaded ? (
     <Loading />
@@ -15,7 +20,7 @@ const Identity = ({ history }) => {
     <>
       <div>{user.name}</div>
       <div>
-        <Button onClick={() => firebase.logout()}>Log Out</Button>
+        <Button onClick={handleLogout}>Log Out</Button>
       </div>
     </>
   ) : (
