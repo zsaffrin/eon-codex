b6b13a2ee35066717e218afb47902fcfca15086d
Zach Saffrin <zach@zachsaffrin.com>
2019-10-29 09:20:22 -0400

    Set up External Home page

---
+++ src/components/Home/ExternalHome.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import { Link, useHistory } from "react-router-dom";
+
+const StyledPage = styled.div`
+  background-image: url("https://res.cloudinary.com/dbg0v7696/image/upload/f_auto,q_auto/v1572134405/eon-codex/Hive_Primus_exterior.png");
+  background-position: top right;
+  background-size: cover;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+`;
+const LoginPanel = styled.div`
+  background: #ccc;
+  border-radius: 0.5em;
+  padding: 1em;
+`;
+
+const ExternalHome = () => {
+  const history = useHistory();
+  return (
+    <StyledPage>
+      <LoginPanel>
+        <Link to="/login">Log In To Explore</Link>
+      </LoginPanel>
+    </StyledPage>
+  );
+};
+
+export default ExternalHome;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,22 +5,14 @@ import { FirebaseContext } from "../../contexts/firebaseContext";
 import { useCurrentUser } from "../../hooks/authHooks";
 import { Loading } from "../ui";
 
+import ExternalHome from "./ExternalHome";
 import InternalHome from "./InternalHome";
 
 const Home = () => {
   const firebase = useContext(FirebaseContext);
   const [user, userLoaded] = useCurrentUser(firebase);
 
-  return !userLoaded ? (
-    <Loading />
-  ) : user ? (
-    <InternalHome />
-  ) : (
-    <div>
-      <h1>Welcome to the Eon Codex</h1>
-      <div>Work is in progress</div>
-    </div>
-  );
+  return !userLoaded ? <Loading /> : user ? <InternalHome /> : <ExternalHome />;
 };
 
 export default Home;
