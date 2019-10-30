2dc0f991af32d6a76066654380cc4322d659da58
Zach Saffrin <zach@zachsaffrin.com>
2019-10-29 21:38:17 -0400

    Set some button styling

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button(({ theme }) => {
+  const { color } = theme;
+  return `
+    -webkit-appearance: none;
+    border-radius: 0.25em;
+    background: ${color.primary};
+    color: #fff;
+    cursor: pointer;
+    font-weight: bold;
+    padding: 0.5em 1em;
+
+    &:hover {
+      background: ${color.primaryLight};
+    }
+  `;
+});
 
 const Button = props => {
-  return <button {...props}>{props.children}</button>;
+  return <StyledButton {...props}>{props.children}</StyledButton>;
 };
 
 export default Button;
