e5748e0d09062b79dc7db00073c6a4557354b110
Zach Saffrin <zach@zachsaffrin.com>
2019-10-29 22:03:42 -0400

    Set some input styling

--- src/components/ui/Input/TextInput.js
+++ src/components/ui/Input/TextInput.js
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledInput = styled.input(({ theme }) => {
+  const { color } = theme;
+  return `
+    border: 1px solid ${color.accent};
+    border-radius: 0.25em;
+    padding: 0.5em;
+    font-size: 0.9em;
+  `;
+});
 
 const TextInput = ({ id, value, onChange }) => {
-  return <input type="text" id={id} value={value} onChange={onChange} />;
+  return <StyledInput type="text" id={id} value={value} onChange={onChange} />;
 };
 
 export default TextInput;
