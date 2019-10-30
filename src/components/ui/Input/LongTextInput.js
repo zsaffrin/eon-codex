f7a6046059c6f2d749702785b7ff83c63137c3e4
Zach Saffrin <zach@zachsaffrin.com>
2019-10-29 22:12:37 -0400

    Style the textarea

--- src/components/ui/Input/LongTextInput.js
+++ src/components/ui/Input/LongTextInput.js
@@ -1,10 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledTextarea = styled.textarea`
-  width: 30em;
-  height: 10em;
-`;
+const StyledTextarea = styled.textarea(({ theme }) => {
+  const { color } = theme;
+  return `
+    border: 1px solid ${color.accent};
+    border-radius: 0.25em;
+    padding: 0.5em;
+    font-size: 0.9em;
+    width: 100%;
+    min-height: 20em;
+  `;
+});
 
 const LongTextInput = ({ id, value, onChange }) => {
   return <StyledTextarea id={id} name={id} value={value} onChange={onChange} />;
