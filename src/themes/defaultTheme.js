f6c705db0db3ab5d8d3b3e712aff9e6170192dce
Zach Saffrin <zach@zachsaffrin.com>
2019-10-30 21:09:37 -0400

    Reformat definition structure; Add inputs vars

--- src/themes/defaultTheme.js
+++ src/themes/defaultTheme.js
@@ -1,20 +1,27 @@
+const space = {
+  thin: "2px",
+  sm: "0.25em",
+  md: "0.5em",
+  lg: "1em",
+  xl: "2em"
+};
+const color = {
+  // Color palette generated on Palx
+  // https://palx.jxnblk.com/1561ad
+  grays: ["#f8f9f9", "#ecedee", "#dee0e3", "#bfc4c8", "#adb3b9", "#818a92"],
+  purples: ["", "", "", "#6115ad"],
+  primary: "#8e44ad",
+  primaryLight: "#AB69C8",
+  accent: "#cfd3d6"
+};
+const inputs = {
+  borderColor: color.accent
+};
+
 const defaultTheme = {
-  space: {
-    thin: "2px",
-    sm: "0.25em",
-    md: "0.5em",
-    lg: "1em",
-    xl: "2em"
-  },
-  color: {
-    // Color palette generated on Palx
-    // https://palx.jxnblk.com/1561ad
-    grays: ["#f8f9f9", "#ecedee", "#dee0e3", "#bfc4c8", "#adb3b9", "#818a92"],
-    purples: ["", "", "", "#6115ad"],
-    primary: "#8e44ad",
-    primaryLight: "#AB69C8",
-    accent: "#cfd3d6"
-  }
+  space,
+  color,
+  inputs
 };
 
 export default defaultTheme;
