8962da143beae811f734c32fe53cba59fa3b5085
Zach Saffrin <zach@zachsaffrin.com>
2019-11-02 19:02:44 -0400

    Add white and black, remove variants

--- src/themes/defaultTheme.js
+++ src/themes/defaultTheme.js
@@ -11,9 +11,9 @@ const color = {
   grays: ["#f8f9f9", "#ecedee", "#dee0e3", "#bfc4c8", "#adb3b9", "#818a92"],
   purples: ["", "", "", "#6115ad"],
   primary: "#8e44ad",
-  primaryLight: "#AB69C8",
-  primaryAltText: "#fff",
-  accent: "#cfd3d6"
+  accent: "#cfd3d6",
+  white: "#f8f9f9",
+  black: "#384048"
 };
 const inputs = {
   borderColor: color.accent
