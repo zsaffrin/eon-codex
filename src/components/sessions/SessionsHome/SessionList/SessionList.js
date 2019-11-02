bf9f76b8f0b130c40b424281cb1a3edbeb9f8d5f
Zach Saffrin <zach@zachsaffrin.com>
2019-11-02 17:50:35 -0400

    Enable support for compact styling

--- src/components/sessions/SessionsHome/SessionList/SessionList.js
+++ src/components/sessions/SessionsHome/SessionList/SessionList.js
@@ -7,15 +7,19 @@ const StyledList = styled.div(({ compact, theme }) => {
   const { space } = theme;
   return `
     display: grid;
-    grid-gap: ${compact ? space.sm : space.md}
+    grid-gap: ${compact ? space.thin : space.md}
   `;
 });
 
 const SessionList = ({ compact, sessions }) => {
   return (
-    <StyledList>
+    <StyledList compact={compact ? 1 : 0}>
       {sessions.map(session => (
-        <SessionListItem compact={compact} key={session.id} session={session} />
+        <SessionListItem
+          compact={compact ? 1 : 0}
+          key={session.id}
+          session={session}
+        />
       ))}
     </StyledList>
   );
--- src/components/sessions/SessionsHome/SessionList/SessionListItem.js
+++ src/components/sessions/SessionsHome/SessionList/SessionListItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { bool } from "prop-types";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -11,11 +12,12 @@ const StyledItem = styled(Link)(({ compact, theme }) => {
     border: 1px solid ${color.primary};
     border-radius: ${space.sm};
     display: grid;
-    grid-template-columns: 10em 1fr;
+    grid-template-columns: ${compact ? "9em" : "10em"} 1fr;
     text-decoration: none;
 
     & > div {
       padding: ${compact ? `${space.thin} ${space.md}` : space.md}
+      font-size: ${compact ? "0.85em" : "inherit"}
     }
 
     &:hover {
@@ -40,7 +42,7 @@ const SessionListItem = ({ compact, session }) => {
   const { date, id, location } = session;
 
   return (
-    <StyledItem to={`/sessions/${id}`} compact={compact}>
+    <StyledItem to={`/sessions/${id}`} compact={compact ? 1 : 0}>
       <DateCell>{formatDate(date.toDate())}</DateCell>
       <div>
         <Lookup collection="gamingLocations" recordId={location} />
