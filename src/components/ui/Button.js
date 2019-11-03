b39bb46954c6845d5f98a240a126ecdfbd0b5103
Zach Saffrin <zach@zachsaffrin.com>
2019-11-03 16:52:02 -0500

    Add Small mode, dynamic color settings

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,25 +1,67 @@
 import React from "react";
 import styled from "styled-components";
+import tinycolor from "tinycolor2";
 
-const StyledButton = styled.button(({ theme }) => {
-  const { color } = theme;
+const StyledButton = styled.button(({ small, theme }) => {
+  const { space } = theme;
   return `
     -webkit-appearance: none;
-    border-radius: 0.25em;
-    background: ${color.primary};
-    color: #fff;
+    border-radius: ${space.sm};
     cursor: pointer;
+    font-size: ${small ? "0.9em" : "1em"};
     font-weight: bold;
-    padding: 0.5em 1em;
+    padding: ${small ? `${space.thin} ${space.md}` : `${space.md} ${space.lg}`};
+  `;
+});
+const ColorButton = styled(StyledButton)(({ primary, disabled, theme }) => {
+  const { color, inputs } = theme;
+  const {
+    buttonColorDefault,
+    buttonColorDisabled,
+    buttonTextColorLight,
+    buttonTextColorDark
+  } = inputs;
+
+  let buttonColor = buttonColorDefault;
+  if (primary) {
+    buttonColor = color.primary;
+  }
+  if (disabled) {
+    buttonColor = buttonColorDisabled;
+  }
+
+  let buttonTextColor = "";
+  if (tinycolor(buttonColor).isLight()) {
+    buttonTextColor = buttonTextColorDark;
+  } else {
+    buttonTextColor = buttonTextColorLight;
+  }
+
+  return `
+    background: ${tinycolor(buttonColor)};
+    color: ${tinycolor(buttonTextColor)};
+    border: 1px solid ${tinycolor(buttonColor).darken(10)};
+    box-shadow: inset 0 1px 0 ${tinycolor(buttonColor).lighten(10)};
+    text-shadow: ${
+      tinycolor(buttonColor).isLight()
+        ? `0 1px 0 ${tinycolor(buttonColor)
+            .lighten(50)
+            .setAlpha(0.25)
+            .toRgbString()}`
+        : `0 -1px 0 ${tinycolor(buttonColor)
+            .darken(50)
+            .setAlpha(0.5)
+            .toRgbString()}`
+    };
 
     &:hover {
-      background: ${color.primaryLight};
+      background: ${tinycolor(buttonColor).darken(15)};
     }
   `;
 });
 
 const Button = props => {
-  return <StyledButton {...props}>{props.children}</StyledButton>;
+  return <ColorButton {...props}>{props.children}</ColorButton>;
 };
 
 export default Button;
