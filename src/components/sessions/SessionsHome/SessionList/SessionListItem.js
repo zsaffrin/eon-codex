7381515485854aa8cb2a4f15ca828e42bd1a6d32
Zach Saffrin <zach@zachsaffrin.com>
2019-11-02 21:34:06 -0400

    Use adjustments to prop color

--- src/components/sessions/SessionsHome/SessionList/SessionListItem.js
+++ src/components/sessions/SessionsHome/SessionList/SessionListItem.js
@@ -6,10 +6,11 @@ import tinycolor from "tinycolor2";
 import { formatDate } from "../../../../utils/dateUtils";
 import { Lookup } from "../../../ui";
 
-const StyledItem = styled(Link)(({ compact, theme }) => {
+const StyledItem = styled(Link)(({ maincolor, compact, theme }) => {
   const { color, space } = theme;
+  const primaryColor = tinycolor(maincolor);
   return `
-    border: 1px solid ${color.primary};
+    border: 1px solid ${primaryColor};
     border-radius: ${space.sm};
     display: grid;
     grid-template-columns: ${compact ? "9em" : "10em"} 1fr;
@@ -21,36 +22,51 @@ const StyledItem = styled(Link)(({ compact, theme }) => {
     }
 
     &:hover {
-      border-color: ${tinycolor(color.primary).lighten(10)};
+      border-color: ${
+        primaryColor.isLight()
+          ? primaryColor.darken(20)
+          : primaryColor.lighten(10)
+      };
     }
   `;
 });
-const DateCell = styled.div(({ theme }) => {
-  const { color, space } = theme;
+const DateCell = styled.div(({ maincolor, theme }) => {
+  const { color } = theme;
+  const primaryColor = tinycolor(maincolor);
+
   return `
-    background: ${color.primary};
-    color: ${tinycolor(color.primary).isLight() ? color.black : color.white};
+    background: ${primaryColor};
+    color: ${color.background};
     font-weight: bold;
 
     ${StyledItem}:hover > & {
-      background: ${tinycolor(color.primary).lighten(10)};
+      background: ${primaryColor.darken(10)};
     }
   `;
 });
-const ContentCell = styled.div(({ theme }) => {
-  const { color } = theme;
+const ContentCell = styled.div(({ maincolor }) => {
+  const primaryColor = tinycolor(maincolor);
+
   return `
-    color: ${color.primary};
+    color: ${primaryColor.darken(10)};
+
+    &:hover {
+      color: ${primaryColor.darken(10)};
+    }
   `;
 });
 
-const SessionListItem = ({ compact, session }) => {
+const SessionListItem = ({ compact, mainColor, session }) => {
   const { date, id, location } = session;
 
   return (
-    <StyledItem to={`/sessions/${id}`} compact={compact ? 1 : 0}>
-      <DateCell>{formatDate(date.toDate())}</DateCell>
-      <ContentCell>
+    <StyledItem
+      to={`/sessions/${id}`}
+      compact={compact ? 1 : 0}
+      maincolor={mainColor}
+    >
+      <DateCell maincolor={mainColor}>{formatDate(date.toDate())}</DateCell>
+      <ContentCell maincolor={mainColor}>
         <Lookup collection="gamingLocations" recordId={location} />
       </ContentCell>
     </StyledItem>
