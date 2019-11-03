fb4fd8a418da5c3bdfa2345f202b674c321bf7c8
Zach Saffrin <zach@zachsaffrin.com>
2019-11-03 17:18:13 -0500

    Render links as Links

--- src/components/ui/Markdown.js
+++ src/components/ui/Markdown.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import { Link } from "react-router-dom";
+
+const RenderLink = props =>
+  props.href.match(/^(https?:)?\/\//) ? (
+    <a href={props.href}>{props.children}</a>
+  ) : (
+    <Link to={props.href}>{props.children}</Link>
+  );
 
 const Markdown = ({ content }) => {
-  return <ReactMarkdown source={content} />;
+  return <ReactMarkdown source={content} renderers={{ link: RenderLink }} />;
 };
 
 export default Markdown;
