b0b994b144e1638ce00de7df6a1b0580429f99c6
Zach Saffrin <zach@zachsaffrin.com>
2019-11-04 22:14:54 -0500

    Cleanup of old approaches

--- src/components/info/InfoHome.js
+++ src/components/info/InfoHome.js
@@ -2,34 +2,22 @@ import React from "react";
 import styled from "styled-components";
 
 import { Breadcrumb, Page } from "../ui";
-import InfoCategory from "./InfoCategory";
 import CategorySelector from "./CategorySelector";
 import RecordSelector from "./RecordSelector";
 import RecordViewer from "./RecordViewer";
 
-const CategoryList = styled.div(({ theme }) => {
-  const { space } = theme;
-  return `
-    display: grid;
-    grid-gap: ${space.lg};
-  `;
-});
-
 const InfoHome = () => {
   return (
     <Page>
-      <Breadcrumb links={[{ label: "Home", target: "/" }, { label: "Info", target: "/info" }]} />
-
+      <Breadcrumb
+        links={[
+          { label: "Home", target: "/" },
+          { label: "Info", target: "/info" }
+        ]}
+      />
       <CategorySelector />
       <RecordSelector />
       <RecordViewer />
-
-      {/* <CategoryList>
-        <InfoCategory title="Places" collectionName="places" />
-        <InfoCategory title="People" collectionName="people" />
-        <InfoCategory title="Groups and Guilds" collectionName="groups" />
-        <InfoCategory title="PCs" collectionName="playerCharacters" />
-      </CategoryList> */}
     </Page>
   );
 };
