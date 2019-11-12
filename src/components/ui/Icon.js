2edd5440748461abca66192b0c6368cc95d67340
Zach Saffrin <zach@zachsaffrin.com>
2019-11-11 21:31:54 -0500

    Pass through props

--- src/components/ui/Icon.js
+++ src/components/ui/Icon.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Icon = ({ name }) => {
-  return <FontAwesomeIcon icon={name} />;
+const Icon = ({ name, ...props }) => {
+  return <FontAwesomeIcon icon={name} {...props} />;
 };
 
 export default Icon;
