64b0b56c13efa969f11973eddf84f8dea9277ae4
Zach Saffrin <zach@zachsaffrin.com>
2019-11-16 14:42:06 -0500

    Add wrap to center-align and cap max width so image doesn't stretch
    Addresses #7

--- src/components/ui/Image.js
+++ src/components/ui/Image.js
@@ -1,16 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
-const ImageWrap = styled.span(({ theme }) => {
+const ImageWrap = styled.span`
+  display: grid;
+  justify-content: center;
+`;
+const Frame = styled.span(({ theme }) => {
   const { color, space } = theme;
   return `
-    display: block;
+    display: inline-block;
     border: 1px solid ${color.accent};
     padding: ${space.md};
   `;
 });
 const StyledImg = styled.img`
-  width: 100%;
+  max-width: 100%;
 `;
 const Caption = styled.span(({ theme }) => {
   const { space } = theme;
@@ -24,10 +28,12 @@ const Caption = styled.span(({ theme }) => {
 const Image = ({ alt, src }) => {
   return (
     <ImageWrap>
-      <StyledImg
-        src={`https://res.cloudinary.com/dbg0v7696/image/upload/f_auto,q_auto/v1572134405/eon-codex/${src}`}
-      />
-      <Caption>{alt}</Caption>
+      <Frame>
+        <StyledImg
+          src={`https://res.cloudinary.com/dbg0v7696/image/upload/f_auto,q_auto/v1572134405/eon-codex/${src}`}
+        />
+        <Caption>{alt}</Caption>
+      </Frame>
     </ImageWrap>
   );
 };
