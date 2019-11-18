c0f02e6f8d7783dccad00fe219df8d5706a2bd66
Zach Saffrin <zach@zachsaffrin.com>
2019-11-17 22:12:26 -0500

    Use the UserContext instead of hook. No longer need to check loaded status

--- src/components/Header/Identity/LoginStatus.js
+++ src/components/Header/Identity/LoginStatus.js
@@ -1,41 +1,31 @@
 import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
 
-import { useCurrentUser } from "../../../hooks/authHooks";
-import { FirebaseContext } from "../../../contexts/firebaseContext";
+import { FirebaseContext, UserContext } from "../../../contexts";
 import { Button, Loading } from "../../ui";
 
 const LoginStatus = () => {
   const firebase = useContext(FirebaseContext);
-  const [user, userLoaded] = useCurrentUser(firebase);
+  const { user } = useContext(UserContext);
   const history = useHistory();
 
   const handleLogout = () => {
     firebase.logout();
   };
 
-  if (!userLoaded) {
-    return <Loading />;
-  }
-  if (user) {
-    return (
-      <div>
-        <Button small onClick={handleLogout}>
-          Log Out
-        </Button>
-      </div>
-    );
-  }
-
-  return (
+  return user ? (
+    <div>
+      <Button small onClick={handleLogout}>
+        Log Out
+      </Button>
+    </div>
+  ) : (
     <div>
       <Button small onClick={() => history.push("/login")}>
         Log In
       </Button>
     </div>
   );
-
-  return <div>{}</div>;
 };
 
 export default LoginStatus;
