1f5ccfac406fef6f586b1b26fd3bc978b4026a80
Zach Saffrin <zach@zachsaffrin.com>
2019-11-21 21:38:40 -0500

    Add path to manage Notes

--- src/components/settings/collections/CollectionsHome.js
+++ src/components/settings/collections/CollectionsHome.js
@@ -3,11 +3,12 @@ import React from "react";
 import { Breadcrumb, Link, Page } from "../../ui";
 
 const collectionLinks = [
+  { label: "Groups", path: "/settings/collection/groups", sort: "name" },
+  { label: "Notes", path: "/settings/collection/notes", sort: "name" },
   { label: "People", path: "/settings/collection/people", sort: "name" },
   { label: "Places", path: "/settings/collection/places", sort: "name" },
   { label: "PCs", path: "/settings/collection/playerCharacters", sort: "name" },
   { label: "Sessions", path: "/settings/collection/sessions", sort: "date" },
-  { label: "Groups", path: "/settings/collection/groups", sort: "name" },
   { label: "Things", path: "/settings/collection/things", sort: "name" }
 ];
 
