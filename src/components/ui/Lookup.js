71ed8fda66ba592d8079c2edad5a7a9a544e67d8
Zach Saffrin <zach@zachsaffrin.com>
2019-11-26 08:31:11 -0500

    Handle blank/null values

--- src/components/ui/Lookup.js
+++ src/components/ui/Lookup.js
@@ -3,8 +3,14 @@ import React from "react";
 import { useDocument } from "../../hooks/firestoreHooks";
 import { Link, Loading } from "../ui";
 
+const nullValues = [null, undefined, ""];
+
 const Lookup = ({ collection, recordId, noLink }) => {
-  const [record, recordLoading] = useDocument(`${collection}/${recordId}`);
+  const [record, recordLoading] = useDocument(
+    `${nullValues.includes(collection) ? " " : collection}/${
+      nullValues.includes(recordId) ? " " : recordId
+    }`
+  );
 
   if (recordLoading) {
     return <Loading inline />;
