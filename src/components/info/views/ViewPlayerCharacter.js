c5429c25f3b635e4bdbda76cc9ceb84c865bf666
Zach Saffrin <zach@zachsaffrin.com>
2019-11-26 11:03:48 -0500

    Include Info and Notes fields

--- src/components/info/views/ViewPlayerCharacter.js
+++ src/components/info/views/ViewPlayerCharacter.js
@@ -5,13 +5,13 @@ import { UserContext } from "../../../contexts";
 import { Link, Lookup, Markdown, Page } from "../../ui";
 
 const ViewPlayerCharacter = ({ record }) => {
-  const { name, player, bio } = record;
+  const { name, player, bio, info, notes } = record;
   const { url } = useRouteMatch();
   const { user } = useContext(UserContext);
 
   return (
     <Page>
-      {user && user.canEdit && (
+      {user.canEdit && (
         <div>
           <Link to={`${url}/edit`}>Edit</Link>
         </div>
@@ -20,7 +20,24 @@ const ViewPlayerCharacter = ({ record }) => {
       <p>
         Played by <Lookup collection="players" recordId={player} noLink />
       </p>
-      <Markdown content={bio} />
+      {notes && user.uid === player && (
+        <div>
+          <h2>My Notes</h2>
+          <Markdown content={notes} />
+        </div>
+      )}
+      {info && (
+        <div>
+          <h2>Info</h2>
+          <Markdown content={info} />
+        </div>
+      )}
+      {bio && (
+        <div>
+          <h2>Backstory</h2>
+          <Markdown content={bio} />
+        </div>
+      )}
     </Page>
   );
 };
