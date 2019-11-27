647441427244832ce5befad617f25aa168fda515
Zach Saffrin <zach@zachsaffrin.com>
2019-11-26 21:03:25 -0500

    Handle null and blank values

--- src/components/ui/Table/CollectionCell.js
+++ src/components/ui/Table/CollectionCell.js
@@ -3,8 +3,14 @@ import React from "react";
 import { useDocument } from "../../../hooks/firestoreHooks";
 import Loading from "../Loading";
 
+const nullValues = [null, undefined, ""];
+
 const CollectionCell = ({ fieldValue, lookup }) => {
-  const [record, recordLoading] = useDocument(`${lookup}/${fieldValue}`);
+  const [record, recordLoading] = useDocument(
+    `${nullValues.includes(lookup) ? " " : lookup}/${
+      nullValues.includes(fieldValue) ? " " : fieldValue
+    }`
+  );
 
   return recordLoading ? <Loading /> : <div>{record && record.name}</div>;
 };
