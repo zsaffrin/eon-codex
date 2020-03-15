6d70bbc5e2a8ecf4bbcfce1a496a3169196b4493
Zach Saffrin <zach@zachsaffrin.com>
2020-03-15 17:27:37 -0400

    Include Action Bar, provide route to EditRecord

--- src/components/info/RecordContent/RecordContent.js
+++ src/components/info/RecordContent/RecordContent.js
@@ -3,24 +3,22 @@ import { useParams } from 'react-router-dom';
 
 import { useDocument } from '../../../hooks/firestoreHooks';
 import { Loading, Page } from '../../ui';
+import ActionBar from './ActionBar';
+import EditRecord from './EditRecord';
 import ViewGroup from './ViewGroup';
 import ViewPerson from './ViewPerson';
 import ViewPlace from './ViewPlace';
 import ViewPlayerCharacter from './ViewPlayerCharacter';
 
 const RecordContent = () => {
-  const { categoryId, recordId } = useParams();
+  const { categoryId, recordId, mode } = useParams();
   const [record, recordLoading] = useDocument(`${categoryId}/${recordId}`);
 
   if (recordLoading) {
     return <Loading />;
   }
   if (!categoryId) {
-    return (
-      <Page>
-        Select a category
-      </Page>
-    );
+    return <Page>Select a category</Page>;
   }
   if (!recordId) {
     return (
@@ -29,26 +27,23 @@ const RecordContent = () => {
       </Page>
     );
   }
-  if (categoryId === 'groups') {
-    return <ViewGroup record={record} />;
-  }
-  if (categoryId === 'people') {
-    return <ViewPerson record={record} />;
-  }
-  if (categoryId === 'places') {
-    return <ViewPlace record={record} />;
-  }
-  if (categoryId === 'playerCharacters') {
-    return <ViewPlayerCharacter record={record} />;
+
+  if (mode && (mode === 'add' || mode === 'edit')) {
+    return (
+      <div>
+        <EditRecord addNew={mode === 'add'} />
+      </div>
+    );
   }
 
   return (
-    <Page>
-      <h1>{record.name}</h1>
-      <div>
-        No view defined for this category type
-      </div>
-    </Page>
+    <div>
+      <ActionBar />
+      {categoryId === 'groups' && <ViewGroup record={record} />}
+      {categoryId === 'people' && <ViewPerson record={record} />}
+      {categoryId === 'places' && <ViewPlace record={record} />}
+      {categoryId === 'playerCharacters' && <ViewPlayerCharacter record={record} />}
+    </div>
   );
 };
 
