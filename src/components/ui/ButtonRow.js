12aa1fc3fc8d9b2d03ab6cf6af8e4a6e227c3acc
Zach Saffrin <zach@zachsaffrin.com>
2020-03-18 22:30:35 -0400

    Fix columns and add align prop

--- src/components/ui/ButtonRow.js
+++ src/components/ui/ButtonRow.js
@@ -1,18 +1,30 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+import {
+  arrayOf, string, oneOfType, node,
+} from 'prop-types';
 
-const Row = styled.div(({ items, theme }) => {
+const Row = styled.div(({ align, items, theme }) => {
   const { space } = theme;
   return `
     display: grid;
-    grid-template-columns: repeat(${items}, auto) 1fr;
+    grid-template-columns: repeat(${items}, auto);
     grid-gap: ${space.sm};
+    justify-content: ${align || 'auto'};
     padding: ${space.md} 0;
   `;
 });
 
-const ButtonRow = ({ children }) => {
-  return <Row items={children.length}>{children}</Row>;
+const ButtonRow = ({ align, children }) => (
+  <Row align={align} items={children.length}>{children}</Row>
+);
+ButtonRow.propTypes = {
+  align: string,
+  children: oneOfType([arrayOf(node), node]),
+};
+ButtonRow.defaultProps = {
+  align: '',
+  children: [],
 };
 
 export default ButtonRow;
