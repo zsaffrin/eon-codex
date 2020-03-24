a57f6d02be362762083d79284c41567692614134
Zach Saffrin <zach@zachsaffrin.com>
2020-03-24 16:01:47 -0400

    Overhaul schema and schemaField management

--- src/components/settings/ManageSchema/FieldsManager.js
+++ src/components/settings/ManageSchema/FieldsManager.js
@@ -1,90 +1,59 @@
-import React, { useContext } from "react";
-import { arrayOf, shape, string } from "prop-types";
-import styled from "styled-components";
+import React, { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 
-import { FirebaseContext } from "../../../contexts/firebaseContext";
-import { useCollection } from "../../../hooks/firestoreHooks";
-import { Button, Loading } from "../../ui";
-import { sortBy } from "../../../utils/dataUtils";
-import Field from "./Field";
+import { useSchemaFields } from '../../../hooks/firestoreHooks';
+import {
+  ButtonRow, Button, Loading, Table,
+} from '../../ui';
 
-const columns = [
-  { key: "key", name: "Key" },
-  { key: "name", name: "Name" },
-  { key: "order", name: "Order" },
-  { key: "type", name: "Type" }
-];
-
-const StyledHeader = styled.div`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-`;
-
-const HeaderCell = styled.div`
-  font-weight: bold;
-`;
-
-const FullWidthCell = styled.div`
-  grid-column: 1 / -1;
-`;
-
-const FieldsManager = ({ schemaName }) => {
-  const [fields, fieldsLoading] = useCollection("schemaFields", [
-    "schema",
-    "==",
-    schemaName
+const FieldsManager = () => {
+  const { schemaName } = useParams();
+  const [columns] = useState([
+    { key: 'key', name: 'Key' },
+    { key: 'name', name: 'Name' },
+    {
+      key: 'order', name: 'Order', type: 'number', align: 'center',
+    },
+    { key: 'type', name: 'Type' },
+    {
+      key: 'showAsBoolean', name: 'Compact', type: 'boolean', align: 'center',
+    },
+    {
+      key: 'showOnTables', name: 'Display', type: 'boolean', align: 'center',
+    },
   ]);
-  const firebase = useContext(FirebaseContext);
+  const [entries, setEntries] = useState(null);
+  const [schemaFields, schemaFieldsLoading] = useSchemaFields(schemaName);
 
-  async function createNewField() {
-    const maxOrder = fields.reduce(
-      (acc, { order }) => (order > acc ? order : acc),
-      0
-    );
-    try {
-      const res = await firebase.db.collection("schemaFields").add({
-        key: "",
-        name: "",
-        order: maxOrder + 1,
-        schema: schemaName
-      });
-      if (res.status === "success") {
-      }
-      if (res.status === "error") {
-        // Implement message display so this works
-        // setMessage(res.result);
-      }
-    } catch (err) {
-      // Implement message display so this works
-      // setMessage(err.message);
+  const history = useHistory();
+
+  // Set up entries
+  useEffect(() => {
+    if (!schemaFieldsLoading && schemaFields && !entries) {
+      setEntries(schemaFields);
     }
-  }
+  }, [schemaFieldsLoading, schemaFields, entries]);
 
-  return fieldsLoading ? (
-    <Loading />
-  ) : (
+  const actions = [
+    {
+      label: 'Edit',
+      action: (e) => history.push(`/settings/schemaField/${e.schema}/${e.id}`),
+    },
+  ];
+
+  return schemaFieldsLoading || !entries ? <Loading /> : (
     <div>
-      <StyledHeader>
-        {columns.map(({ key, name }) => (
-          <HeaderCell key={`${key}Header`}>{name}</HeaderCell>
-        ))}
-      </StyledHeader>
-      {sortBy(fields, "order").map(field => (
-        <Field key={field.key} data={field} />
-      ))}
-      <FullWidthCell>
-        <Button small onClick={createNewField}>
-          +
-        </Button>
-      </FullWidthCell>
+      <Table
+        columns={columns}
+        entries={entries}
+        actions={actions}
+        orderKey="order"
+      />
+      <ButtonRow align="start">
+        <Button tiny onClick={() => history.push(`/settings/schemaField/${schemaName}/add`)}>Add Field</Button>
+      </ButtonRow>
     </div>
   );
-};
-FieldsManager.propTypes = {
-  fields: arrayOf(shape({ key: string, name: string, type: string }))
-};
-FieldsManager.defaultProps = {
-  fields: []
 };
 
 export default FieldsManager;
---
+++ src/components/settings/ManageSchema/ManageSchema.js
@@ -0,0 +1,110 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { FirebaseContext } from '../../../contexts/firebaseContext';
+import { useSchema } from '../../../hooks/firestoreHooks';
+import {
+  Button, H, Input, Loading, Page, VerticalList,
+} from '../../ui';
+import FieldsManager from './FieldsManager';
+
+const PageSection = styled.div(({ theme }) => {
+  const { space } = theme;
+  return `
+    padding-bottom: ${space.md};
+    padding-top: ${space.md};
+  `;
+});
+
+const ManageSchema = () => {
+  const { schemaName } = useParams();
+  const [schema, schemaLoading] = useSchema(schemaName);
+  const [workingSchema, setWorkingSchema] = useState(null);
+  const firebase = useContext(FirebaseContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!schemaLoading && schema && !workingSchema) {
+      setWorkingSchema(schema);
+    }
+  }, [schemaLoading, schema, workingSchema]);
+
+  const handleFieldChange = (e) => {
+    setWorkingSchema({
+      ...workingSchema,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  async function saveSchemaDetails() {
+    try {
+      const { id, ...rest } = workingSchema;
+      const res = await firebase.updateDoc(`schemas/${id}`, rest);
+      if (res.status === 'success') {
+        // This needs to redirect to the schema front page
+        // Good opportunity to use new ReactRouter
+      }
+      if (res.status === 'error') {
+        // Implement message display so this works
+        // setMessage(res.result);
+      }
+    } catch (err) {
+      // Implement message display so this works
+      // setMessage(err.message);
+    }
+  }
+
+  const fields = workingSchema ? [
+    {
+      label: 'API Key',
+      content: (
+        <Input
+          type="text"
+          id="apiKey"
+          value={workingSchema.apiKey}
+          onChange={handleFieldChange}
+        />
+      ),
+    },
+    {
+      label: 'Name',
+      content: (
+        <Input
+          type="text"
+          id="name"
+          value={workingSchema.name}
+          onChange={handleFieldChange}
+        />
+      ),
+    },
+  ] : [];
+
+  return !workingSchema ? (
+    <Loading />
+  ) : (
+    <Page fullWidth>
+      <h1>Manage Schema</h1>
+      <PageSection>
+        <VerticalList items={fields} />
+        <Button small onClick={saveSchemaDetails}>
+          Save Details
+        </Button>
+      </PageSection>
+      <PageSection>
+        <H l={2}>Fields</H>
+        <FieldsManager schemaName={schemaName} />
+      </PageSection>
+      <PageSection>
+        <Button
+          small
+          onClick={() => history.push(`/settings/collection/${schemaName}`)}
+        >
+          Done
+        </Button>
+      </PageSection>
+    </Page>
+  );
+};
+
+export default ManageSchema;
