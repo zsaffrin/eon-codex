fa22fcba80de96708600785690803d5587062f32
Zach Saffrin <zach@zachsaffrin.com>
2020-03-25 22:25:43 -0400

    Formatting, add setDoc method

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-UrQ-fXWu3OcldY0xU8MU8-ztFYYe46c",
-  authDomain: "eon-codex-23d99.firebaseapp.com",
-  databaseURL: "https://eon-codex-23d99.firebaseio.com",
-  projectId: "eon-codex-23d99",
-  storageBucket: "eon-codex-23d99.appspot.com",
-  messagingSenderId: "156955843402",
-  appId: "1:156955843402:web:10f4a8824adb6c00"
+  apiKey: 'AIzaSyD-UrQ-fXWu3OcldY0xU8MU8-ztFYYe46c',
+  authDomain: 'eon-codex-23d99.firebaseapp.com',
+  databaseURL: 'https://eon-codex-23d99.firebaseio.com',
+  projectId: 'eon-codex-23d99',
+  storageBucket: 'eon-codex-23d99.appspot.com',
+  messagingSenderId: '156955843402',
+  appId: '1:156955843402:web:10f4a8824adb6c00',
 };
 
 class Firebase {
@@ -22,8 +22,8 @@ class Firebase {
   login(email, password) {
     return this.auth
       .signInWithEmailAndPassword(email, password)
-      .then(res => ({ status: "success", result: res }))
-      .catch(err => ({ status: "error", result: err.message }));
+      .then((res) => ({ status: 'success', result: res }))
+      .catch((err) => ({ status: 'error', result: err.message }));
   }
 
   logout() {
@@ -35,45 +35,59 @@ class Firebase {
       .collection(collection)
       .doc(doc)
       .get()
-      .then(doc => (doc.exists ? doc.data() : null))
-      .catch(err => console.error(err.message));
+      .then((doc) => (doc.exists ? doc.data() : null))
+      .catch((err) => console.error(err.message));
   }
 
   getCollection(collection) {
     return this.db
       .collection(collection)
       .get()
-      .then(snapshot =>
-        snapshot.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data()
-        }))
-      )
-      .catch(err => console.error(err.message));
+      .then((snapshot) => snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })))
+      .catch((err) => console.error(err.message));
   }
 
   addDoc(collection, doc) {
     return this.db
       .collection(collection)
-      .add(doc)
-      .then(res => ({ status: "success", result: res }))
-      .catch(err => ({ status: "error", result: err.message }));
+      .add({
+        ...doc,
+        created: new Date(),
+        modified: new Date(),
+      })
+      .then((res) => ({ status: 'success', result: res }))
+      .catch((err) => ({ status: 'error', result: err.message }));
+  }
+
+  setDoc(collection, id, doc) {
+    return this.db
+      .collection(collection)
+      .doc(id)
+      .set(doc)
+      .then((res) => ({ status: 'success', result: res }))
+      .catch((err) => ({ status: 'error', result: err.message }));
   }
 
   updateDoc(query, value) {
     return this.db
       .doc(query)
-      .update(value)
-      .then(res => ({ status: "success", result: res }))
-      .catch(err => ({ status: "error", result: err.message }));
+      .update({
+        ...value,
+        modified: new Date(),
+      })
+      .then((res) => ({ status: 'success', result: res }))
+      .catch((err) => ({ status: 'error', result: err.message }));
   }
 
   deleteDoc(query) {
     return this.db
       .doc(query)
       .delete()
-      .then(res => ({ status: "success", result: res }))
-      .catch(err => ({ status: "error", result: err.message }));
+      .then((res) => ({ status: 'success', result: res }))
+      .catch((err) => ({ status: 'error', result: err.message }));
   }
 }
 
