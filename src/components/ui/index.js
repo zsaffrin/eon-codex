e214c94f404272e908466291ca506a699cb4246a
Zach Saffrin <zach@zachsaffrin.com>
2020-04-22 19:47:34 -0400

    Include new components for export

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -2,9 +2,12 @@ import Breadcrumb from './Breadcrumb';
 import { Button, ButtonRow } from './Button';
 import H from './H';
 import Icon from './Icon';
+import Image from './Image';
 import Input from './Input';
 import Link from './Link';
 import Loading from './Loading';
+import Lookup from './Lookup';
+import Markdown from './Markdown';
 import Modal from './Modal';
 import Page from './Page';
 import Table from './Table';
@@ -16,9 +19,12 @@ export {
   ButtonRow,
   H,
   Icon,
+  Image,
   Input,
   Link,
   Loading,
+  Lookup,
+  Markdown,
   Modal,
   Page,
   Table,
