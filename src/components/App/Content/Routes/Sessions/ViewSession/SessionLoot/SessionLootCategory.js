f1bba349150c3f6ed59f65bb578567b6b9d64917
Zach Saffrin <zach@zachsaffrin.com>
2020-04-22 22:51:20 -0400

    Create SessionLoot component

---
+++ src/components/App/Content/Routes/Sessions/ViewSession/SessionLoot/SessionLootCategory.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { arrayOf, shape, string } from 'prop-types';
+import styled from 'styled-components';
+
+import { sortBy } from '../../../../../../../utils';
+import { Modal, Table } from '../../../../../../ui';
+import { EditRecord } from '../../../../shared';
+
+const StyledCategory = styled.div(({ theme }) => {
+  const { color, space } = theme;
+  return `
+    border: 1px solid ${color.primary};
+    border-radius: 4px;
+    padding: ${space.sm};
+  `;
+});
+const TitleBar = styled.div`
+  font-weight: bold;
+  text-align: center;
+  text-transform: uppercase;
+`;
+
+const fieldKeysToInclude = ['name', 'claim', 'comments', 'whereFound'];
+
+const SessionLootCategory = ({ fields, items, title }) => {
+  const [editLootItem, setEditLootItem] = useState(null);
+
+  const toggleEditLootItem = (item) => {
+    setEditLootItem(editLootItem ? null : item);
+  };
+
+  const columns = sortBy(fields, 'displayOrder').filter(({ key }) => fieldKeysToInclude.includes(key));
+  const actions = [
+    { label: 'Edit', action: toggleEditLootItem },
+  ];
+
+  return (
+    <StyledCategory>
+      {editLootItem && (
+        <Modal>
+          <EditRecord
+            schemaId="loot"
+            onCancel={toggleEditLootItem}
+            onSaveSuccess={toggleEditLootItem}
+            recordData={editLootItem}
+          />
+        </Modal>
+      )}
+      <TitleBar>{title}</TitleBar>
+      <Table
+        columns={columns}
+        entries={items}
+        actions={actions}
+      />
+    </StyledCategory>
+  );
+};
+SessionLootCategory.propTypes = {
+  items: arrayOf(shape({})),
+  title: string,
+  fields: arrayOf(shape({})),
+};
+SessionLootCategory.defaultProps = {
+  items: [],
+  title: '',
+  fields: [],
+};
+
+export default SessionLootCategory;
