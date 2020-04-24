c6dc702929193e2100b1bef540dd70c1f26e580b
Zach Saffrin <zach@zachsaffrin.com>
2020-04-23 23:16:19 -0400

    Load the user in context to feed the hook, not vice versa

--- src/contexts/currentUserContext.js
+++ src/contexts/currentUserContext.js
@@ -1,12 +1,45 @@
-import React, { createContext } from 'react';
+import React, {
+  createContext, useContext, useEffect, useState,
+} from 'react';
 import { arrayOf, node, oneOfType } from 'prop-types';
 
-import { useCurrentUser } from '../hooks';
+import { FirebaseContext } from './firebaseContext';
 
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-  const { user, userLoaded } = useCurrentUser();
+  const firebase = useContext(FirebaseContext);
+  const [user, setUser] = useState(null);
+  const [userLoaded, setUserLoaded] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth.onAuthStateChanged((userData) => {
+      async function getDetail() {
+        const detail = await firebase.getDoc(
+          'players',
+          userData ? userData.uid : ' ',
+        );
+        return detail;
+      }
+
+      if (userData) {
+        getDetail().then((detail) => {
+          setUser({
+            uid: userData.uid,
+            ...detail,
+          });
+          setUserLoaded(true);
+        });
+      } else {
+        setUser(userData);
+        setUserLoaded(true);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [firebase]);
 
   return (
     <UserContext.Provider value={{ user, userLoaded }}>
