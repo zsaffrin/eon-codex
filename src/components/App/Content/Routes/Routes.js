7380ac6bf83350b9ca464569e593ee9863d91e8f
Zach Saffrin <zach@zachsaffrin.com>
2020-04-23 23:20:50 -0400

    Include routes to Player and Info sections

--- src/components/App/Content/Routes/Routes.js
+++ src/components/App/Content/Routes/Routes.js
@@ -4,15 +4,17 @@ import { Route, Switch } from 'react-router-dom';
 import FourOhFour from './404';
 import Home from './Home';
 import Info from './Info';
+import Player from './Player';
 import Sessions from './Sessions';
 import Setup from './Setup';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
-    <Route path="/info" component={Info} />
+    <Route path="/info/:collectionId?/:recordId?" component={Info} />
     <Route path="/sessions" component={Sessions} />
     <Route path="/setup" component={Setup} />
+    <Route path="/player" component={Player} />
     <Route component={FourOhFour} />
   </Switch>
 );
