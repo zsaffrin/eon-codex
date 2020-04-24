ab730ac1a6884db1793564e87eb781ca27bba98a
Zach Saffrin <zach@zachsaffrin.com>
2020-04-23 23:15:49 -0400

    Don't load new user, get it from the context

--- src/hooks/useCurrentUser.js
+++ src/hooks/useCurrentUser.js
@@ -1,44 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useContext } from 'react';
 
-import useFirebase from './useFirebase';
+import { UserContext } from '../contexts/currentUserContext';
 
 const useCurrentUser = () => {
-  const firebase = useFirebase();
-  const [user, setUser] = useState(null);
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    const unsubscribe = firebase.auth.onAuthStateChanged((userData) => {
-      async function getDetail() {
-        const detail = await firebase.getDoc(
-          'players',
-          userData ? userData.uid : ' ',
-        );
-        return detail;
-      }
-
-      if (userData) {
-        getDetail().then((detail) => {
-          setUser({
-            uid: userData.uid,
-            ...detail,
-          });
-          setLoaded(true);
-        });
-      } else {
-        setUser(userData);
-        setLoaded(true);
-      }
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, [firebase]);
+  const { user, userLoaded } = useContext(UserContext);
 
   return {
     user,
-    userLoaded: loaded,
+    userLoaded,
   };
 };
 
