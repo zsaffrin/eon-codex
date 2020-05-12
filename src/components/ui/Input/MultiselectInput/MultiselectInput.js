340956d567d651649f4fe5979e7efc1e20ae24f8
Zach Saffrin <zach@zachsaffrin.com>
2020-05-11 22:04:00 -0400

    Clean up lookup use of SchemaId as Collection ID

--- src/components/ui/Input/MultiselectInput/MultiselectInput.js
+++ src/components/ui/Input/MultiselectInput/MultiselectInput.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { string } from 'prop-types';
 
-import { useCollection, useSchema } from '../../../../hooks';
+import { useCollection } from '../../../../hooks';
 import Loading from '../../Loading';
 import Multiselect from './Multiselect';
 
 const MultiselectInput = (props) => {
   const { lookup } = props;
-  const [schema, schemaLoading] = useSchema(lookup || ' ');
-  const [collection, collectionLoading] = useCollection(schema && schema.collection ? schema.collection : ' ');
+  const [collection, collectionLoading] = useCollection(lookup || ' ');
 
   const choices = collection ? collection.map(({ id, name }) => ({ id, label: name })) : [];
 
-  return schemaLoading || collectionLoading ? <Loading /> : (
+  return collectionLoading ? <Loading /> : (
     <Multiselect choices={choices} />
   );
 };
