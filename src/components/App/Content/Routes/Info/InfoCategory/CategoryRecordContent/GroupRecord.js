bef90b8db563de69ae7e407154bbaff5de964118
Zach Saffrin <zach@zachsaffrin.com>
2020-07-15 23:09:52 -0400

    Include PlayerNotes

--- src/components/App/Content/Routes/Info/InfoCategory/CategoryRecordContent/GroupRecord.js
+++ src/components/App/Content/Routes/Info/InfoCategory/CategoryRecordContent/GroupRecord.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { shape, string } from 'prop-types';
+import { shape, string, arrayOf } from 'prop-types';
 
-import { Markdown } from '../../../../../../ui';
+import { Auth, Markdown } from '../../../../../../ui';
+import { PlayerNotes } from '../../../../shared';
 
-const GroupRecord = ({ record }) => {
+const GroupRecord = ({ record, notes }) => {
   const { desc } = record;
 
   return (
     <>
       {desc && <Markdown content={desc} />}
-      {/* <Notes /> */}
+      <Auth level={1}>
+        <PlayerNotes
+          collection="groups"
+          article={record.id}
+          notes={notes}
+        />
+      </Auth>
     </>
   );
 };
@@ -18,11 +25,13 @@ GroupRecord.propTypes = {
   record: shape({
     desc: string,
   }),
+  notes: arrayOf(shape({})),
 };
 GroupRecord.defaultProps = {
   record: {
     desc: '',
   },
+  notes: [],
 };
 
 export default GroupRecord;
