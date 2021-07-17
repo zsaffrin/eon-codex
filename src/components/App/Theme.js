02f58999522ffc3e7e583ea798412947c8462946
Zach Saffrin <zach@zachsaffrin.com>
2021-07-16 22:25:14 -0400

    Add TODO comments for theme switcher

--- src/components/App/Theme.js
+++ src/components/App/Theme.js
@@ -7,6 +7,10 @@ import { themeDefaultDark } from '../../themes';
 const Theme = ({ children }) => {
   const [theme, setTheme] = useState(themeDefaultDark);
   
+  //TODO: Create theme switcher
+  //Should be able to do include this as a method on the theme object
+  //unless this messes with styled-components
+
   return (
     <ThemeProvider theme={theme}>
       {children}
