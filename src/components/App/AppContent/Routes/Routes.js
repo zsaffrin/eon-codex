3b503603b16b3294d96dc21726d74bdc1860f486
Zach Saffrin <zach@zachsaffrin.com>
2021-07-16 23:15:23 -0400

    Add Home component and Route

--- src/components/App/AppContent/Routes/Routes.js
+++ src/components/App/AppContent/Routes/Routes.js
@@ -1,10 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 
 import FourOhFour from './404';
+import Home from './Home';
 
 const Routes = () => {
   return (
     <Switch>
+      <Route path="/" exact component={Home} />
+      
       <Route component={FourOhFour} />
     </Switch>
   );
