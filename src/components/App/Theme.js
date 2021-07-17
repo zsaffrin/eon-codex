eb37bbbabf8f0f3c9d18efb6554d2bacebc8328c
Zach Saffrin <zach@zachsaffrin.com>
2021-07-16 22:35:45 -0400

    Stop loading setTheme
    Just creates eslint warnings, will bring it back once actually ready to use it

--- src/components/App/Theme.js
+++ src/components/App/Theme.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'styled-components';
 import { themeDefaultDark } from '../../themes';
 
 const Theme = ({ children }) => {
-  const [theme, setTheme] = useState(themeDefaultDark);
+  const [theme] = useState(themeDefaultDark);
   
   //TODO: Create theme switcher
   //Should be able to do include this as a method on the theme object
