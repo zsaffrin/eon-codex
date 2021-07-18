451c05ac873d59d15f85bd704da3a2e5bd83f745
Zach Saffrin <zach@zachsaffrin.com>
2021-07-18 13:35:53 -0400

    Create useDocument

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,9 +1,13 @@
+import useCollection from './useCollection';
+import useDocument from './useDocument';
 import useFirebase from './useFirebase';
 import useForm from './useForm';
 import useMessage from './useMessage';
 import useUser from './useUser';
 
 export {
+  useCollection,
+  useDocument,
   useFirebase,
   useForm,
   useMessage,
