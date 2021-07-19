060b6142107c01abeb416928ec357c2667b48068
Zach Saffrin <zach@zachsaffrin.com>
2021-07-18 22:27:18 -0400

    Build out form fields, unique key generation/validation

--- src/components/App/AppContent/Routes/Dashboard/Campaigns/NewCampaign.js
+++ src/components/App/AppContent/Routes/Dashboard/Campaigns/NewCampaign.js
@@ -1,12 +1,82 @@
-import { Button, ButtonRow, H, Page } from '../../../../../ui';
+import { useState } from 'react';
+
+import { useBoolState, useCollection } from '../../../../../../hooks';
+import { Button, ButtonRow, H, Input, Loading, Page, VerticalList } from '../../../../../ui';
+import { stringToKey } from '../../../../../../utilities';
 
 const NewCampaign = ({ close }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    campaignKey: '',
+  });
+  const [isKeyUnique, setIsKeyUnique] = useBoolState(true);
+  const [campaigns, isCampaignsLoading] = useCollection('campaigns');
+
+  const handleFieldChange = ({ id, value }) => {
+    setFormData({
+      ...formData,
+      [id]: value,
+    });
+    if (id === 'campaignKey') {
+      const existingKeys = campaigns 
+        ? campaigns.filter(c => c.key === value)
+        : [];
+      setIsKeyUnique(existingKeys.length === 0);
+    }
+  };
+
+  const generateKey = () => {
+    if (!formData.campaignKey) {
+      setFormData({
+        ...formData,
+        campaignKey: stringToKey(formData.name),
+      });
+    }
+  };
+
+  if (isCampaignsLoading) {
+    return <Loading />;
+  }
+
+  const listItems = [
+    {
+      label: 'Campaign Name',
+      content: (
+        <Input
+          id="name"
+          value={formData.name}
+          onChange={handleFieldChange}
+          onBlur={generateKey}
+        />
+      ),
+    },
+    {
+      label: 'Campaign Key',
+      content: (
+        <Input
+          id="campaignKey"
+          value={formData.campaignKey}
+          onChange={handleFieldChange}
+        />
+      ),
+      detail: (
+        <>
+          <div>URL for the campaign. Must be unique.</div>
+          {!isKeyUnique && <div>Key is already in use. Please choose another.</div>}
+        </>
+      ),
+    }
+  ];
+
   return (
     <Page>
       <H l={1} centered>New Campaign</H>
-      <ButtonRow>
-        <Button onClick={close}>Cancel</Button>
-      </ButtonRow>
+      <form>
+        <VerticalList items={listItems} />
+        <ButtonRow>
+          <Button onClick={close}>Cancel</Button>
+        </ButtonRow>
+      </form>
     </Page>
   );
 };
