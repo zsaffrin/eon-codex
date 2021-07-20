c7c568c15aa3b00917954228cb8409e994c18f65
Zach Saffrin <zach@zachsaffrin.com>
2021-07-19 21:22:56 -0400

    Set up form validation and campaign creation
    Inserting a Campaign will also insert a Player Owner record for that Campaign for the creating User

--- src/components/App/AppContent/Routes/Dashboard/Campaigns/NewCampaign.js
+++ src/components/App/AppContent/Routes/Dashboard/Campaigns/NewCampaign.js
@@ -1,27 +1,35 @@
 import { useState } from 'react';
 
-import { useBoolState, useCollection } from '../../../../../../hooks';
+import { useCollection, useFirebase, useMessage, useUser } from '../../../../../../hooks';
 import { Button, ButtonRow, H, Input, Loading, Page, VerticalList } from '../../../../../ui';
 import { stringToKey } from '../../../../../../utilities';
 
 const NewCampaign = ({ close }) => {
+  const [user] = useUser();
+  const firebase = useFirebase();
   const [formData, setFormData] = useState({
     name: '',
     campaignKey: '',
   });
-  const [isKeyUnique, setIsKeyUnique] = useBoolState(true);
+  const [isKeyUnique, setIsKeyUnique] = useState(true);
   const [campaigns, isCampaignsLoading] = useCollection('campaigns');
+  const [message, setMessage] = useMessage();
 
   const handleFieldChange = ({ id, value }) => {
-    setFormData({
-      ...formData,
-      [id]: value,
-    });
     if (id === 'campaignKey') {
       const existingKeys = campaigns 
         ? campaigns.filter(c => c.key === value)
         : [];
       setIsKeyUnique(existingKeys.length === 0);
+      setFormData({
+        ...formData,
+        campaignKey: stringToKey(value),
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [id]: value,
+      });
     }
   };
 
@@ -31,6 +39,64 @@ const NewCampaign = ({ close }) => {
         ...formData,
         campaignKey: stringToKey(formData.name),
       });
+    }
+  };
+
+  const isFormValid = () => {
+    if (!formData.name) {
+      setMessage('error', 'Campaign Name is required');
+      return false;
+    }
+    if (!formData.campaignKey) {
+      setMessage('error', 'Campaign Key is required');
+      return false;
+    }
+    if (!isKeyUnique) {
+      setMessage('error', 'Campaign Key must be unique');
+      return false;
+    }
+
+    return true;
+  };
+
+  const insertCampaign = () => {
+    return firebase.addDoc('campaigns', {
+      name: formData.name,
+      key: formData.campaignKey,
+      createdBy: user.uid,
+    });
+  };
+  const insertPlayer = (campaignId) => {
+    return firebase.addDoc('players', {
+      campaign: campaignId,
+      name: user.name,
+      isOwner: true,
+      isEditor: true,
+      user: user.uid,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage();
+    if (isFormValid()) {
+      try {
+        const addCampaignResult = await insertCampaign();
+        if (addCampaignResult.status === 'error') {
+          setMessage('error', addCampaignResult.result);
+        } else if (addCampaignResult.status === 'success') {
+          const addPlayerResult = await insertPlayer(addCampaignResult.result.id);
+          if (addPlayerResult.status === 'error') {
+            setMessage('error', addPlayerResult.result);
+          } else if (addPlayerResult.status === 'success') {
+            close();
+          } else {
+            setMessage('error', 'Unknown add player error');  
+          }
+        } else {
+          setMessage('error', 'Unknown add campaign error');
+        }
+      } catch (err) { setMessage('error', err, true); }
     }
   };
 
@@ -71,9 +137,11 @@ const NewCampaign = ({ close }) => {
   return (
     <Page>
       <H l={1} centered>New Campaign</H>
-      <form>
+      {message}
+      <form onSubmit={handleSubmit}>
         <VerticalList items={listItems} />
         <ButtonRow>
+          <Button primary type="submit">Create Campaign</Button>
           <Button onClick={close}>Cancel</Button>
         </ButtonRow>
       </form>
