04ae5d7a521e90f6c9470aa8047234e5c6c64638
Zach Saffrin <zach@zachsaffrin.com>
2021-07-20 22:06:53 -0400

    Create randomKey utility

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,11 +1,13 @@
 import formatDate from './formatDate';
 import maxFieldValue from './maxFieldValue';
+import randomKey from './randomKey';
 import sortBy from './sortBy';
 import stringToKey from './stringToKey';
 
 export {
   formatDate,
   maxFieldValue,
+  randomKey,
   sortBy,
   stringToKey,
 };
