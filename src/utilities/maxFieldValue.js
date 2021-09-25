365988b80c6665ed6391a2227ed8aa1ef497871a
Zach Saffrin <zach@zachsaffrin.com>
2021-09-24 22:15:42 -0400

    Add documentation

--- src/utilities/maxFieldValue.js
+++ src/utilities/maxFieldValue.js
@@ -1,3 +1,12 @@
+/**
+ * Gets the maximum value of a given field 
+ * within a given array of objects
+ *
+ * @param {array} items The array of objects to evaluate
+ * @param {string} fieldKey The field key to evaluate
+ * @param {boolean} min [Optional] Evaluate the min value instead of max
+ * @return {number} Max (or min) value result of evaluation
+ */
 const maxFieldValue = (items, fieldKey, min) => items.reduce((acc, item) => {
   if (min) {
     return (
