8e9ca0567d5fcd3faa374b6d118872ee8dc9807b
Zach Saffrin <zach@zachsaffrin.com>
2021-09-24 22:18:36 -0400

    Add usage documentation

--- src/hooks/useMessage.js
+++ src/hooks/useMessage.js
@@ -5,6 +5,14 @@ import { Message } from '../components/ui';
 const useMessage = () => {
   const [message, setMessage] = useState(null);
 
+  /**
+   * Method to set message
+   * 
+   * @param {string} messageType Message type: success|warn|error|info[default]
+   * @param {string} messageContent Description of param
+   * @param {boolean} raw [Optional] JSON.stringify content
+   * @return None
+   */
   const handleMessageUpdate = (messageType, messageContent, raw) => {
     setMessage(!messageType && !messageContent
       ? null
