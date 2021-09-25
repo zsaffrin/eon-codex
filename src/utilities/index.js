c38b9bc604ca72e72e895479b469a693786181f1
Zach Saffrin <zach@zachsaffrin.com>
2021-09-24 22:18:05 -0400

    Export isValidEmail and objectifyArray utilities

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,12 +1,16 @@
 import formatDate from './formatDate';
+import isValidEmail from './isValidEmail';
 import maxFieldValue from './maxFieldValue';
+import objectifyArray from './objectifyArray';
 import randomKey from './randomKey';
 import sortBy from './sortBy';
 import stringToKey from './stringToKey';
 
 export {
   formatDate,
+  isValidEmail,
   maxFieldValue,
+  objectifyArray,
   randomKey,
   sortBy,
   stringToKey,
