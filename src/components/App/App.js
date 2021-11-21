34ca9090e744162aa911ce2e51d0afb39424333f
Zach Saffrin <zach@zachsaffrin.com>
2021-11-21 16:31:12 -0500

    Remove test page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,11 @@
 import { FirebaseProvider } from '../../contexts';
 
 import GlobalStyle from './GlobalStyle';
-import AppTest from './AppTest';
 
 const App = () => {
   return (
     <FirebaseProvider>
       <GlobalStyle />
-      <h1>EonCodex 0.6.0</h1>
-      <AppTest />
     </FirebaseProvider>
   );
 };
