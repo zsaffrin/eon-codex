fdd61708f4ab87a5e2855a3b3e12a49cb78faa0e
Zach Saffrin <zach@zachsaffrin.com>
2022-08-11 21:49:23 -0400

    Use sortable list for categories

--- src/components/App/AppContent/Routes/Campaign/Info/EditCategories/CategoryList.js
+++ src/components/App/AppContent/Routes/Campaign/Info/EditCategories/CategoryList.js
@@ -1,14 +1,14 @@
 import { arrayOf, shape } from 'prop-types';
 import { FaPlus } from 'react-icons/fa';
 
-import { maxFieldValue } from '../../../../../../../utilities';
+import { maxFieldValue, sortByKey } from '../../../../../../../utilities';
 import { useCampaign, useFirebase } from '../../../../../../../hooks';
 import { Button, ButtonRow, ItemList } from '../../../../../../ui';
 import CategoryListItem from './CategoryListItem';
 
 const CategoryList = ({ categories }) => {
   const { id: campaignId } = useCampaign();
-  const { addDocument } = useFirebase();
+  const { addDocument, updateDocument } = useFirebase();
 
   const addNewCategory = () => {
     const newDisplayOrder = maxFieldValue(categories, 'displayOrder') + 1;
@@ -22,9 +22,26 @@ const CategoryList = ({ categories }) => {
     
     addDocument('articleCategories', docToAdd);
   };
+
+  const handleOrderChange = ({ recordId, targetLocation }) => {
+    categories.forEach((category) => {
+      if (category.id === recordId) {
+        updateDocument('articleCategories', recordId, { 
+          displayOrder: targetLocation,
+        });
+      } else {
+        if (category.displayOrder >= targetLocation) {
+          updateDocument('articleCategories', category.id, {
+            displayOrder: category.displayOrder + 1,
+          });
+        }
+      }
+    });
+  };
   
   const listItems = categories.map(category => ({
     key: category.id,
+    displayOrder: category.displayOrder,
     content: (
       <CategoryListItem category={category} />
     )
@@ -32,7 +49,12 @@ const CategoryList = ({ categories }) => {
   
   return (
     <div>
-      <ItemList items={listItems} />
+      <ItemList
+        items={sortByKey(listItems, 'displayOrder')}
+        sortable
+        sortKey="displayOrder"
+        handleOrderChange={handleOrderChange}
+      />
       <ButtonRow>
         <Button
           icon={<FaPlus />}
