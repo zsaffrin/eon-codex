653197d297bce27d4131efb5540f9a84ed833ff3
Zach Saffrin <zach@zachsaffrin.com>
2022-08-25 22:18:47 -0400

    Include character images

--- src/components/App/AppContent/Routes/Campaign/Characters/Characters.js
+++ src/components/App/AppContent/Routes/Campaign/Characters/Characters.js
@@ -1,23 +1,67 @@
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaUserCircle } from 'react-icons/fa';
+import { AdvancedImage } from '@cloudinary/react';
+import { thumbnail } from '@cloudinary/url-gen/actions/resize';
+import styled from 'styled-components';
 
-import { useCampaign, useToggledModal } from '../../../../../../hooks';
+import { useCampaign, useCloudinary, useToggledModal } from '../../../../../../hooks';
 import { Box, Button, H, ItemList, Page, PageHeader } from '../../../../../ui';
 import AddCharacter from './AddCharacter';
+
+const StyledItem = styled.div(({ theme }) => {
+  const { space } = theme;
+
+  return `
+    padding: ${space.md};
+    display: grid;
+    align-items: center;
+    grid-gap: ${space.lg};
+    grid-template-columns: min-content 1fr;
+  `;
+});
+const ImagePlaceholder = styled.div(({ theme }) => {
+  const { color } = theme;
+
+  return `
+    height: 50px;
+    width: 50px;
+    display: grid;
+    align-items: center;
+    justify-items: center;
+    font-size: 2rem;
+    color: ${color.gray};
+  `;
+});
 
 const Characters = () => {
   const { key: campaignKey, characters } = useCampaign();
   const [addModal, toggleAddModal] = useToggledModal(AddCharacter);
+  const { cloudinaryUrlGen } = useCloudinary();
 
-  const listItems = characters.map(({ id, name, playerName }) => ({
-    key: id,
-    content: (
-      <>
-        <H l={3} compact>{name}</H>
-        <div>{`Played by ${playerName}`}</div>
-      </>
-    ),
-    to: `/campaign/${campaignKey}/character/${id}`,
-  }));
+  const listItems = characters.map(({ id, name, playerName, imagePublicId }) => {
+    const charImg = cloudinaryUrlGen.image(imagePublicId);
+    charImg.resize(thumbnail().width(50).height(50));
+    
+    return ({
+      key: id,
+      content: (
+        <StyledItem>
+          {imagePublicId
+            ? <AdvancedImage cldImg={charImg} />
+            : (
+                <ImagePlaceholder>
+                  <FaUserCircle />
+                </ImagePlaceholder>
+              )
+          }
+          <div>
+            <H l={3} compact>{name}</H>
+            <div>{`Played by ${playerName}`}</div>
+          </div>
+        </StyledItem>
+      ),
+      to: `/campaign/${campaignKey}/character/${id}`,
+    });
+  });
   
   return (
     <Page>
